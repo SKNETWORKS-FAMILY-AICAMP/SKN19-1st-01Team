ef506070fa2e368ac1f9c2e250a6737038576c8d
jjoggoddalgi <simon882@naver.com>
2025-09-03 11:41:29 +0900

    1
    기본 파일 구성

---
+++ home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(
+    page_title="환경보호에 1조",
+    page_icon="😊"
+)
+
+st.title("환경보호에 1조")
+st.sidebar.success("Select a page above")
